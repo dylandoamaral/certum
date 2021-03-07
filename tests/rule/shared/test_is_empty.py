565f4bd74bd5b3a779411eb51d7986f3a9478902
dylandoamaral <do.amaral.dylan@gmail.com>
2021-03-07 12:56:16 +0100

    Add more tests

--- tests/rule/shared/test_is_empty.py
+++ tests/rule/shared/test_is_empty.py
@@ -23,3 +23,12 @@ def test_is_empty_failure():
     with pytest.raises(CertumException) as error:
         validator.check()
     assert_error(error, "[x] => The path x is not empty.")
+
+
+def test_unknown_path():
+    """Ensuring that the rule doesn't start if the path is unknown."""
+    obj = {}
+    validator = ensure(obj).respects(that("x").is_empty())
+    with pytest.raises(CertumException) as error:
+        validator.check()
+    assert_error(error, "[x] => The path is missing.")
